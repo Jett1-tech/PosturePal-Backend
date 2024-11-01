c66c4d232eebb881643058f13b0af480e7451692
huynhgocnhan <huynhngocnhan29@gmail.com>
2024-11-01 18:35:13 +0700

    image dir

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     const uri =
-      "mongodb+srv://root:123@cluster0.ld5nn.mongodb.net/PosturePal?retryWrites=true&w=majority&appName=Cluster0";
+      "mongodb://127.0.0.1/PosturePal";
     await mongoose.connect(uri);
     console.log("Connect to DB Successfully");
   } catch (error) {
